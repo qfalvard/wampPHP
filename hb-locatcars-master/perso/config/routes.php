5be74762a7880f5d62476b1ea4f184db7d8541b9
qfalvard <qufalvard@gmail.com>
2020-02-10 16:23:53 +0100

    premier index cars

--- hb-locatcars-master/perso/config/routes.php
+++ hb-locatcars-master/perso/config/routes.php
@@ -6,10 +6,7 @@
 
 $router = $container->getRouter();
 
-// Quand je vais sur "/hello", j'effectue l'action suivante, qui est définie
-// dans la fonction anonyme notée juste après : function() { /* action */ }
-$router->get('/hello', function(){
-    echo 'hello world!!';
-});
+$router->setNamespace('App\Controller');
+$router->get('/cars', 'CarsController@index');
 
 $router->run(); // NE JAMAIS OUBLIE SINON LE ROUTER NE SE LANCE PAS!!!
---
+++ hb-locatcars-master/perso/src/controller/CarsController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controller;
+
+class CarsController {
+
+    public function index() {
+        include __DIR__ . '/../../template/cars/index.php';
+    }
+}
